d9b24a7741555f8ba9672bba27208a11a00810e8
Eemeli Martti <eemeli.martti@live.fi>
2020-08-02 13:30:55 +0300

    7.1.-7.3.

---
+++ part07/routed-anecdotes/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './App'
+
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
